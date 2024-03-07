4613bb211d4468635084cb9176562bb11be76a99
Jongabee <jonatanmosqueda@gmail.com>
2024-03-07 04:31:26 -0300

    M2 L3

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,25 @@
 console.log(tempData);
+const moviesContainer= document.getElementById("movies-container");
+
+function renderFilms (movie){
+    const movieElement = document.createElement ("article")
+    const containerMovie = document.createElement ("div")
+    movieElement.classList.add("movie")
+    movieElement.classList.add("divMovie")
+
+    movieElement.innerHTML = `<img class= "card-image" src="${movie.poster}" alt = "${movie.title}">`
+
+    containerMovie.innerHTML = `
+    <h1> ${movie.title}</h1>
+    <p><strong>Director:</strong> ${movie.director}</p>
+    <p><strong>Duración:</strong> ${movie.duration}</p>
+    <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
+    <p><strong>Rate:</strong> ${movie.rate}</p>
+    <p><strong>Año:</strong> ${movie.year}</p>
+
+    `
+    moviesContainer.appendChild(movieElement)
+    movieElement.appendChild(containerMovie)
+}
+$.get('https://students-api.2.us-1.fl0.io/movies',(data)=> data.forEach(renderFilms))
+// tempData.forEach(renderFilms)

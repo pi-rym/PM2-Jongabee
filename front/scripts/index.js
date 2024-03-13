324d8057505b7cded3b9002feff3c38257ca61dd
Jongabee <jonatanmosqueda@gmail.com>
2024-03-12 22:11:58 -0300

    M2 HW 06

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,7 +1,19 @@
-//const axios = require ("axios")//
+
 const renderFilms = require("./runderFilms")
 
 // $.get('https://students-api.2.us-1.fl0.io/movies',(data)=> data.forEach(renderFilms))
 
+// $.get('https://students-api.up.railway.app/movies', (data)=> data.forEach(renderFilms))
 
-$.get('https://students-api.up.railway.app/movies', (data)=> data.forEach(renderFilms))
+
+const axios = require ('axios')
+
+const fetchData = async ()=> {
+    try {
+        const {data} = await axios.get ('https://students-api.up.railway.app/movies')
+        data.forEach(renderFilms)
+    } catch(error){
+        console.log(error.mesaje);
+    }
+}
+fetchData()
